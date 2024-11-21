5b0d02983da2674303583b22804ea03b9392c8fb
Miguel <A01383674@tec.mx>
2024-11-20 19:18:08 -0600

    Perfil base

--- app/src/main/java/com/example/museopapalote/Profile.kt
+++ app/src/main/java/com/example/museopapalote/Profile.kt
@@ -1,12 +1,267 @@
 package com.example.museopapalote
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import java.io.InputStream
 
 @Composable
 fun Profile(navController: NavHostController) {
-    Button(onClick = { navController.navigate("Home") }) {
-        Text("Go to Home")
+    val context = LocalContext.current
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var bitmap by remember { mutableStateOf<ImageBitmap?>(null) }
+    val defaultRating = 5 // Valor predeterminado para el rating
+    val defaultImage = R.drawable.obra_1
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.data?.let { uri ->
+                imageUri = uri
+                val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+                bitmap = BitmapFactory.decodeStream(inputStream)?.asImageBitmap()
+            }
+        }
     }
+
+    Image(
+        painter = painterResource(id = R.drawable.fondoperfil),
+        contentDescription = "Fondo de perfil",
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Imagen de perfil y el nombre
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 8.dp)
+            ) {
+                // Imagen de perfil
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clickable {
+                            val intent = Intent(Intent.ACTION_PICK).apply {
+                                type = "image/*"
+                            }
+                            launcher.launch(intent)
+                        }
+                ) {
+                    if (bitmap != null) {
+                        Image(
+                            bitmap = bitmap!!,
+                            contentDescription = "Profile Picture",
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    } else {
+                        Image(
+                            painter = painterResource(id = R.drawable.userdefault),
+                            contentDescription = "Default Profile Picture",
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column {
+                    Text(
+                        text = "¡Buenos días!",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Text(
+                        text = "Miguel",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                }
+            }
+            Divider(
+                color = Color.Black,
+                thickness = 2.dp,
+                modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                // Fondo blanco
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f) // 50%
+                        .clip(RoundedCornerShape(16.dp)) // Esquinas redondeadas
+                        .background(Color.White) // Color de fondo
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = "Tus Favoritos:",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth() // 50%
+                        .clip(RoundedCornerShape(16.dp)) // Esquinas redondeadas
+                        .background(Color.White) // Color de fondo
+                        .padding(16.dp)
+                ) {
+                    Column {
+                        // Item 1
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 16.dp)
+                        ) {
+                            Divider(
+                                color = Color.Black,
+                                thickness = 3.dp,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.padding(vertical = 8.dp)
+                            ) {
+                                // Imagen predeterminada si no se encuentra la imagen
+                                Image(
+                                    painter = painterResource(id = defaultImage),
+                                    contentDescription = "Default Obra",
+                                    modifier = Modifier
+                                        .size(180.dp) // Tamaño
+                                        .padding(end = 8.dp) // Espacio entre la imagen y el RatingBar
+                                )
+                                // Rating Bar
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(start = 8.dp)
+                                ) {
+                                    repeat(5) { index ->
+                                        Icon(
+                                            imageVector = Icons.Default.Star,
+                                            contentDescription = "Star $index",
+                                            tint = if (index < defaultRating) Color.Yellow else Color.Gray, // Todas las estrellas amarillas
+                                            modifier = Modifier.size(24.dp)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 16.dp)
+                        ) {
+                            Divider(
+                                color = Color.Black,
+                                thickness = 3.dp,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.padding(vertical = 8.dp)
+                            ) {
+                                // Imagen
+                                Image(
+                                    painter = painterResource(id = R.drawable.obra_2),
+                                    contentDescription = "Obra 2",
+                                    modifier = Modifier
+                                        .size(180.dp)
+                                        .padding(end = 8.dp)
+                                )
+                                // Rating Bar
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(start = 8.dp)
+                                ) {
+                                    repeat(5) { index ->
+                                        Icon(
+                                            imageVector = Icons.Default.Star,
+                                            contentDescription = "Star $index",
+                                            tint = if (index < 4) Color.Yellow else Color.Gray,
+                                            modifier = Modifier.size(24.dp)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Button(
+            onClick = { navController.navigate("Home") },
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(16.dp)
+        ) {
+            Text("Go to Home")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ProfilePreview() {
+    val navController = rememberNavController()
+    Profile(navController = navController)
 }
